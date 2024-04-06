9b03dcf421f0e5c0a822b07bad25b81a5bdf5e78
XardoX <jurek.gab.jg@gmail.com>
2024-04-06 17:38:21 +0200

    timeline testing

--- Assets/Samples/Timeline/1.7.6/Customization Samples/Tween/TweenBehaviour.cs
+++ Assets/Samples/Timeline/1.7.6/Customization Samples/Tween/TweenBehaviour.cs
@@ -7,9 +7,16 @@ namespace Timeline.Samples
     // Runtime representation of a Tween clip.
     public class TweenBehaviour : PlayableBehaviour
     {
+        public Vector3 startPosition;
+        public Vector3 endPosition;
+        public Vector3 startRotation;
+        public Vector3 endRotation;
+        public Vector3 startScale;
+        public Vector3 endScale;
+
+
         public Transform startLocation;
         public Transform endLocation;
-
         public bool shouldTweenPosition;
         public bool shouldTweenRotation;
 
