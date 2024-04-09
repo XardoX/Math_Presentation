74b23781bec696897c848686c50b746d3b43bd88
XardoX <jurek.gab.jg@gmail.com>
2024-04-09 18:49:31 +0200

    menu button prefab

---
+++ Assets/Scripts/Chart/MethodSwitcher.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+namespace MathPresentation
+{
+    public class MethodSwitcher : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform chartParent;
+    }
+}
---
+++ Assets/Scripts/UI/MenuButtonJuice.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
+
+public class MenuButtonJuice :MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [Header("Juice Settings")]
+    [SerializeField]
+    private float hoverScale = 1.1f;
+
+    [SerializeField]
+    private float enterDuration = 0.25f;
+
+    [SerializeField]
+    private Ease enterEase = Ease.Linear;
+
+    [SerializeField]
+    private float exitDuration = 0.25f;
+
+    [SerializeField]
+    private Ease exitEase = Ease.Linear;
+
+    [Header("References")]
+    [SerializeField]
+    private Image image;
+
+
+    private float startWidth;
+
+    private void Awake()
+    {
+        Material mat = Instantiate(image.material);
+
+        image.material = mat;
+        startWidth = mat.GetFloat("_Width");
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.DOScale(hoverScale, enterDuration).SetEase(enterEase);
+        image.material.DOFloat(startWidth * hoverScale, "_Width", enterDuration)
+            .SetEase(enterEase);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.DOScale(1f, exitDuration).SetEase(exitEase);
+        image.material.DOFloat(startWidth, "_Width", exitDuration)
+            .SetEase(exitEase);
+    }
+}
