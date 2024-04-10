1641ca32f4c9544485e5b4c0061c9b64db6f0146
XardoX <jurek.gab.jg@gmail.com>
2024-04-10 16:22:41 +0200

    soft mask plugin

---
+++ Assets/Scripts/Misc/CutoutMaskUI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+
+public class CutoutMaskUI : Image
+{
+    public override Material materialForRendering
+    {
+        get
+        {
+            // get a copy of the base material or you going to F*** up the whole project
+            Material material = new Material(base.materialForRendering);
+            material.SetInt("_StencilComp", (int)CompareFunction.NotEqual);
+            return material;
+        }
+    }
+}
